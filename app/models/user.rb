cc902ba5d1b6a1513e6e10b6b910d11e3043426f
stanley akyea <stanley.akyea@gmail.com>
2019-07-20 09:38:01 +0000

    Add stock restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  
+  # check if stock is already added to user's portfolio
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    
+    # ckeck if user is tracking this stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+  
+  # check if user is under the stock limit of 10
+  def under_stock_limit?
+    user_stocks.count < 10
+  end
+  
+  # check to dd stock if above two conditions are met
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
 end
