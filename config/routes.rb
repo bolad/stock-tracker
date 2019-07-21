2b1f0d4e5afddc66905d361db2cb16a68b137ca9
stanley akyea <stanley.akyea@gmail.com>
2019-07-21 13:42:49 +0000

    Add UI views for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
-  get 'welcome/index'
+  
+  root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   resources :user_stocks, only: [:create, :destroy]
